615e651aa7a89bc43dd74990896c849fad5eb799
thitipongr <thitipong.r.psn@gmail.com>
2024-04-06 16:24:06 +0700

    Connected database

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import debug from "debug";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 import productsRouter from "./src/router/productRouter.js";
+import mysql from "mysql";
+import db_config from "./db_config.json" with { type: "json" };
 
 const app = express();
 const PORT = process.env.PORT;
@@ -18,4 +20,17 @@ app.use("/products", productsRouter);
 
 app.listen(PORT, () => {
   debug(`Server running at <http://localhost:${PORT}>`);
+});
+
+const con = mysql.createConnection({
+  host: db_config.host,
+  port: db_config.port,
+  user: db_config.user,
+  password: db_config.password,
+  database: db_config.database,
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Database Connected!");
 });
